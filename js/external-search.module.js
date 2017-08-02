0d6f61f5e817748fc70a4aca50dba7febbf7305e
budak <thatbudakguy@gmail.com>
2017-08-02 13:46:36 -0700

    reorganize module

---
+++ js/external-search.module.js
@@ -0,0 +1,41 @@
+angular
+  .module('externalSearch', [])
+  .value('searchTargets', [])
+  .component('prmFacetAfter', {
+      bindings: { parentCtrl: '<' },
+      controller: function () {
+        this.parentCtrl.facetService.results.unshift({
+            name: 'External Search',
+            displayedType: 'exact',
+            limitCount: 0,
+            facetGroupCollapsed: false,
+            values: undefined
+        })
+      }]
+  })
+  .component('prmFacetExactAfter', {
+      bindings: { parentCtrl: '<' },
+      template: `
+      <div ng-if="name === 'External Search'">
+          <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">
+              <div class="section-content animate-max-height-variable">
+                  <div class="md-chips md-chips-wrap">
+                      <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">
+                          <div class="md-chip-content layout-row" role="button" tabindex="0">
+                              <strong dir="auto" title="{{ target.name }}">
+                                  <a ng-href="{{ target.url + target.mapping(search) }}" target="_blank">
+                                      <img ng-src="{{ target.img }}" width="22" height="22" style="vertical-align:middle;"> {{ target.name }}
+                                  </a>
+                              </strong>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>`,
+      controller: ['$scope', '$location', 'searchTargets', function ($scope, $location, searchTargets) {
+        $scope.name = this.parentCtrl.facetGroup.name
+        $scope.search = $location.search().query
+        $scope.targets = searchTargets
+      }]
+  })
